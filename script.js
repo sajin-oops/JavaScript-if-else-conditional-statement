b6fa5c54654ef2ef718811311e7e8dcba324b2b1
A.K.SAJIN <108419506+sajin-oops@users.noreply.github.com>
2023-10-07 13:34:07 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,12 @@
+// if else - conditional statement
+
+let pwd_correct = true
+if(pwd_correct)
+{
+  console.log("You are logged in  successfully")
+}
+else
+{
+  console.log("unverified")
+}
+console.log("End")
